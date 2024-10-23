83465325cc480ae4d43e4b970631ce2e985f8d35
Lorenzo De Bie <Lorenzo.DeBie@fednot.be>
2024-10-23 11:47:38 +0200

    User SP instead of managed identity

--- versions.tf
+++ versions.tf
@@ -11,6 +11,6 @@ terraform {
     storage_account_name = "axxesworkshoptfstates"                # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                             # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     # key                  = "lorenzo-dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-    use_oidc              = true                                   # Can also be set via `ARM_USE_MSI` environment variable.
+    use_azuread_auth = true
   }
 }
