46b74255e1f84b24c695d6d86d2a5c388fb5fac8
Lorenzo De Bie <Lorenzo.DeBie@fednot.be>
2024-10-23 20:37:27 +0200

    Fix typo in vnet name

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "azurerm_virtual_network" "vn" {
   resource_group_name = data.azurerm_resource_group.rg-tfworkshops.name
   location = data.azurerm_resource_group.rg-tfworkshops.location
 
-  name = "${var.prefix}-${var.prefix}-vn"
+  name = "${var.prefix}-${var.env_prefix}-vn"
   address_space = [ "10.0.0.0/24" ]
 }
