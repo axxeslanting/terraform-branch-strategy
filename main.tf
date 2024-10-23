10b680f31457a1d8351850ba0e8c35e0ba9825d0
Lorenzo De Bie <Lorenzo.DeBie@fednot.be>
2024-10-23 22:19:41 +0200

    Added resources for AKS cluster

--- main.tf
+++ main.tf
@@ -9,3 +9,28 @@ resource "azurerm_virtual_network" "vn" {
   name = "${var.prefix}-${var.env_prefix}-vn"
   address_space = [ "10.0.0.0/24" ]
 }
+
+resource "azurerm_subnet" "nodepool-sn" {
+  name = "nodepool-sn"
+  resource_group_name = data.azurerm_resource_group.rg-tfworkshops.name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  address_prefixes = ["10.1.0.0/16"]
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.prefix}-${var.env_prefix}-aks"
+  location            = data.azurerm_resource_group.rg-tfworkshops.location
+  resource_group_name = data.azurerm_resource_group.rg-tfworkshops.name
+
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_D2_v3"
+    type = "VirtualMachineScaleSets"
+    vnet_subnet_id = azurerm_subnet.nodepool-sn.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
