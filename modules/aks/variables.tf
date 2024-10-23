157630cc864c5b252063d11169795c7a1b9acd3b
Lorenzo De Bie <Lorenzo.DeBie@fednot.be>
2024-10-23 23:34:55 +0200

    Moved AKS and Postgres to modules

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -7,13 +7,3 @@ variable "env_prefix" {
   type = string
   description = "environment prefix for all resources"
 }
-
-variable "postgres_admin_password" {
-  type = string
-  sensitive = true
-}
-
-variable "postgres_server_zone" {
-  type = string
-  default = "2"
-}
---
+++ modules/postgres/variables.tf
@@ -0,0 +1,45 @@
+variable "prefix" {
+  type = string
+  description = "prefix value for all resources"
+}
+
+variable "env_prefix" {
+  type = string
+  description = "environment prefix for all resources"
+}
+
+
+variable "postgres_admin_login" {
+  type = string
+  default = "psqladmin"
+}
+
+variable "postgres_admin_password" {
+  type = string
+  sensitive = true
+}
+
+variable "server_zone" {
+  type = string
+  default = "2"
+}
+
+variable "server_storage" {
+  type = number
+  default = 32768
+}
+
+variable "storage_tier" {
+  type = string
+  default = "P4"
+}
+
+variable "storage_auto_grow" {
+  type = bool
+  default = true
+}
+
+variable "server_sku" {
+  type = string
+  default = "B_Standard_B1ms"
+}
