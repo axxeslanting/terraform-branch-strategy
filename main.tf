3fea129c90a66557a1a08f3076aa771beb9086c8
Lewis Lanting <lewis.lanting@axxes.com>
2024-10-24 11:36:26 +0200

    add aks

--- main.tf
+++ main.tf
@@ -14,16 +14,16 @@ resource "azurerm_virtual_network" "vn" {
   address_space = ["10.0.0.0/8"]
 }
 
-# module "aks" {
-#   source = "./modules/aks"
+module "aks" {
+  source = "./modules/aks"
 
-#   prefix = var.prefix
-#   env_prefix = var.env_prefix
-#   resource_group_name = data.azurerm_resource_group.rg-tfworkshops.name
-#   virtual_network_name = azurerm_virtual_network.vn.name
+  prefix = var.prefix
+  env_prefix = var.env_prefix
+  resource_group_name = data.azurerm_resource_group.rg-tfworkshops.name
+  virtual_network_name = azurerm_virtual_network.vn.name
 
-#   depends_on = [ azurerm_virtual_network.vn ]
-# }
+  depends_on = [ azurerm_virtual_network.vn ]
+}
 
 # module "postgres" {
 #   source = "./modules/postgres"
