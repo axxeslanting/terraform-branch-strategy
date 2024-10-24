e8b7f786c2e08b327b077bbf5b15fe9b6ed5d17b
Lewis Lanting <lewis.lanting@axxes.com>
2024-10-24 11:11:31 +0200

    initial vnet for resources

---
+++ variables.tf
@@ -0,0 +1,19 @@
+# variable "prefix" {
+#   type = string
+#   description = "prefix value for all resources"
+# }
+
+# variable "env_prefix" {
+#   type = string
+#   description = "environment prefix for all resources"
+# }
+
+# variable "postgres_admin_password" {
+#   type = string
+#   sensitive = true
+# }
+
+# variable "postgres_server_zone" {
+#   type = string
+#   default = "2"
+# }
